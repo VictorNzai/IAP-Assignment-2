fb1fb29105de3e147619901d3c65adfac83c8ff0
Victorurl <166705594+VictorNzai@users.noreply.github.com>
2024-10-08 08:06:59 +0300

    Add headings.php with signUpBanner method

---
+++ contents/headings.php
@@ -0,0 +1,16 @@
+<?php
+
+class headings
+{
+    // Example method
+    public function signUpBanner()
+    {
+        echo "<h1>Sign Up</h1>";
+        ?>
+                <div class="container text-center text-light bg-primary my-5 p-5 rounded">
+                    <h1 class="text-white">Internet Application: Assignment II</h1>
+                    <p class="lead text-light">Use OOP to create a Bootstrap Form that collects information from a user, stores it in a database, and retrieves the data as well.</p>
+                </div>
+        <?php
+    }
+}
