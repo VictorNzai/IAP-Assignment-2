36167dda2e055725534080a8832f8e5bd0d3edf8
Victorurl <166705594+VictorNzai@users.noreply.github.com>
2024-10-08 08:07:23 +0300

    Add constants.php with database connection details

---
+++ includes/constants.php
@@ -0,0 +1,8 @@
+<?php
+// Creating constants
+define("dbType","PDO");
+define("hostName","localhost");
+define("dbPort","3306");
+define("hostUser","root");
+define("hostPass","rehanais2cool");
+define("dbName","user_registration");
