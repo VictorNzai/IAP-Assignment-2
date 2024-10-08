36e41a903c9a6bb95ccfffee55fab5736d960a89
Victorurl <166705594+VictorNzai@users.noreply.github.com>
2024-10-08 09:01:06 +0300

    Add escapeValues method for input data sanitization

--- global/fncs.php
+++ global/fncs.php
@@ -1,7 +1,11 @@
 <?php
 class fncs {
-    public function sanitizeInput($data) {
-        return htmlspecialchars(strip_tags($data));
+    // Add this method if you intend to sanitize input data
+    public function escapeValues($value) {
+        return htmlspecialchars(strip_tags($value), ENT_QUOTES, 'UTF-8');
     }
 }
+
 ?>
+
+
